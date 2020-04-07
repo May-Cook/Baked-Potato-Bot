8b3a071aaccde9f154ce78bfb7b29c723f803a6d
BenRCook <40598265+BenRCook@users.noreply.github.com>
2020-04-08 00:38:12 +0100

    added all verses

--- bakedPotatoBot.py
+++ bakedPotatoBot.py
@@ -3,19 +3,23 @@ import os
 import random
 
 import discord
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+newVerse = True
 
-# adviceList = [
-#     "Wash your hands and stay indoors"
+currenterse = None
 
-    
-# ]
+adviceList = [
+    ["Wash your hands and stay indoors.", "Only go to grocery stores."],
+    ["Keep some distance, Make some space.", "Remember not to touch your face."],
+    ["Do be good. Don\'t be bad.", "Do be happy. Don\'t be sad."],
+    ["Do be early. Don\'t be late.", "Allways eat what\'s on your plate."],   
+]
 
 
 @client.event # this is called a decorator
@@ -24,14 +28,19 @@ async def on_ready():
 
 @client.event
 async def on_message(message): 
-    print(message.content)
-    print(message.author)
-    print(message)
-    print("\n\n\n")
+    global newVerse
+    global currentVerse
 
     if (message.content == "!potato"):
-    
-        await message.channel.send("Wash your hands, Don\'t touch your face.")
+        print(message.author)
+        if (newVerse == True):
+            currentVerse = random.choice(adviceList)
+            await message.channel.send(currentVerse[0])
+            newVerse = False
+        else:
+            await message.channel.send(currentVerse[1])
+            newVerse = True
+
 
 
 client.run("Njk3MTI3NDE3NjMzOTY0MjIz.Xozc1A.EdQL_bGry8JLApjOeFy6IKcLrwY")
