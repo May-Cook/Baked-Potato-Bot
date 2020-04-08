9f8a6408613dce50b242c300db4a95916e9b6b7b
BenRCook <40598265+BenRCook@users.noreply.github.com>
2020-04-08 12:07:07 +0100

    created Potato() class

--- bakedPotatoBot.py
+++ bakedPotatoBot.py
@@ -10,17 +10,33 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-newVerse = True
 
-currenterse = None
+class Potato:
+    def __init__(self):
+        self.adviceList = [
+            ["Wash your hands and stay indoors.", "Only go to grocery stores."],
+            ["Keep some distance, Make some space.", "Remember not to touch your face."],
+            ["Do be good. Don\'t be bad.", "Do be happy. Don\'t be sad."],
+            ["Do be early. Don\'t be late.", "Allways eat what\'s on your plate."],   
+        ]
+        self.newVerse = True
+        self.currentVerse = None
 
-adviceList = [
-    ["Wash your hands and stay indoors.", "Only go to grocery stores."],
-    ["Keep some distance, Make some space.", "Remember not to touch your face."],
-    ["Do be good. Don\'t be bad.", "Do be happy. Don\'t be sad."],
-    ["Do be early. Don\'t be late.", "Allways eat what\'s on your plate."],   
-]
+    def getResponse(self):
+        if (self.newVerse == True):
+            self.currentVerse = random.choice(self.adviceList)
+            self.newVerse = False
+            return self.currentVerse[0]
+        else:
+            self.newVerse = True
+            return self.currentVerse[1]
 
+
+
+
+
+
+potato = Potato()
 
 @client.event # this is called a decorator
 async def on_ready():
@@ -28,19 +44,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message): 
-    global newVerse
-    global currentVerse
+    global potato
 
     if (message.content == "!potato"):
-        print(message.author)
-        if (newVerse == True):
-            currentVerse = random.choice(adviceList)
-            await message.channel.send(currentVerse[0])
-            newVerse = False
-        else:
-            await message.channel.send(currentVerse[1])
-            newVerse = True
-
+        print(str(message.author) + ": !potato")
+        await message.channel.send(potato.getResponse())
 
 
 client.run("Njk3MTI3NDE3NjMzOTY0MjIz.Xozc1A.EdQL_bGry8JLApjOeFy6IKcLrwY")
